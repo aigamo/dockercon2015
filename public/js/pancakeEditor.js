887e278a1eb245af9daaddf5f3793cb9740e821a
aigamo <aigamo@172.22.145.99>
2015-06-20 16:11:31 -0700

    add api connection

--- public/js/pancakeEditor.js
+++ public/js/pancakeEditor.js
@@ -1,21 +1,29 @@
 
 var pancakeJs = {
 		
-		getSheet : funciton(){
+		// edit for your environment
+		endpoint : "http://localhost:8080",
+		
+		getSheet : function(){
 			
 		},
 		
-		getFlavor : function(){
+		getNginxFlavor : function(){
+			$.getJSON(this.endpoint + "/data/nginx_flavor.json", function(json){
+				console.log(json);
+				  alert("JSON Data: " + json.nginx.from.value);
+				});
 			
-		}
+		},
 		
-		parseJson : funciton(json){
+		parseJson : function(json){
 			
 		}
 
 	
 }
 
+pancakeJs.getNginxFlavor();
 
 
 
