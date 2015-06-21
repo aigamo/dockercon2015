9da6ebd26506bbb4e6144af8c54d5ba57843dd10
aigamo <aigamo@172.22.145.99>
2015-06-20 18:24:00 -0700

    add flavor color

--- public/js/pancakeEditor.js
+++ public/js/pancakeEditor.js
@@ -1,30 +1,45 @@
-
 var pancakeJs = {
-		
-		// edit for your environment
-		endpoint : "http://localhost:8080",
-		
-		getSheet : function(){
-			
-		},
-		
-		getNginxFlavor : function(){
-			$.getJSON(this.endpoint + "/data/nginx_flavor.json", function(json){
-				console.log(json);
-				  alert("JSON Data: " + json.nginx.from.value);
-				});
-			
-		},
-		
-		parseJson : function(json){
-			
-		}
 
-	
+	// edit for your environment
+	endpoint : "http://localhost:8080",
+
+	init : function() {
+		this.getRunSheet();
+		this.getNginxFlavor();
+	},
+
+	getRunSheet : function(){
+		$.ajax({
+			url : this.endpoint + "/data/from_sheet.json",
+		}).done(function(data) {
+			var sheetTag = "<ul>";
+			$.each(data.from, function(i, val) {
+				$(sheet_ul).append($("<li class="+val.sheet_name+">" + val.value + "</li>"));
+			});
+		}).fail(function(data) {
+			console.log(data);
+		});
+		
+	},
+	getNginxFlavor : function() {
+		$.ajax({
+			url : this.endpoint + "/data/nginx_flavor.json",
+		}).done(function(data) {
+			console.log(data);
+			var sheetTag = "<ul>";
+			$.each(data.nginx, function(i, val) {
+				$(flavor_ul).append($("<li class="+val.sheet_name+">" + val.value + "</li>"));
+			});
+		}).fail(function(data) {
+			console.log(data);
+		});
+
+	},
+
+	showFlavorSheets : function(data) {
+
+	}
+
 }
 
-pancakeJs.getNginxFlavor();
-
-
-
-
+pancakeJs.init();
