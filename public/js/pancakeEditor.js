5074356cb14770d1b0a54bd578d960ee10ea18a5
aigamo <aigamo@172.22.145.99>
2015-06-20 20:48:20 -0700

    add sortable plugin

--- public/js/pancakeEditor.js
+++ public/js/pancakeEditor.js
@@ -6,38 +6,50 @@ var pancakeJs = {
 	init : function() {
 		this.getRunSheet();
 		this.getNginxFlavor();
+		this.sortablePlugin();
 	},
 
-	getRunSheet : function(){
+	getRunSheet : function() {
 		$.ajax({
 			url : this.endpoint + "/data/from_sheet.json",
-		}).done(function(data) {
-			var sheetTag = "<ul>";
-			$.each(data.from, function(i, val) {
-				$(sheet_ul).append($("<li class="+val.sheet_name+">" + val.value + "</li>"));
-			});
-		}).fail(function(data) {
-			console.log(data);
+		}).done(
+				function(data) {
+					$.each(data.from, function(i, val) {
+						$(sheet_ul).append(
+								$("<li class=" + val.sheet_name + ">"
+										+ val.value + "</li>"));
+					});
+					$("ul.droptrue").sortable({
+						connectWith : "ul"
+					});
+				}).fail(function(data) {
 		});
-		
+
 	},
 	getNginxFlavor : function() {
 		$.ajax({
 			url : this.endpoint + "/data/nginx_flavor.json",
-		}).done(function(data) {
-			console.log(data);
-			var sheetTag = "<ul>";
-			$.each(data.nginx, function(i, val) {
-				$(flavor_ul).append($("<li class="+val.sheet_name+">" + val.value + "</li>"));
-			});
-		}).fail(function(data) {
+		}).done(
+				function(data) {
+					$.each(data.nginx, function(i, val) {
+						$(flavor_ul).append(
+								$("<li class=" + val.sheet_name + ">"
+										+ val.value + "</li>"));
+					});
+					$("ul.droptrue").sortable({
+						connectWith : "ul"
+					});	
+				}).fail(function(data) {
 			console.log(data);
 		});
 
 	},
+	sortablePlugin : function() {
+		$("ul.droptrue").sortable({
+			connectWith : "ul"
+		});
 
-	showFlavorSheets : function(data) {
-
+		$("#sheet_ul, #flavor_ul, #editor_ul").disableSelection();
 	}
 
 }
